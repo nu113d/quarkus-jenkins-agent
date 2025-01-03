f8c23a14a7bd7aa5b1c981a84d77a36dc61fffe5
Vaggelis Stoikos <103531438+nu113d@users.noreply.github.com>
2025-01-03 12:37:48 +0200

    this may work

--- vars/helm.groovy
+++ vars/helm.groovy
@@ -2,6 +2,6 @@ def packHelmChart(){
      sh 'helm package .'
 }
 
-def pushChart(String repoUrl){
+def pushChart(def repoUrl){
     sh 'curl -u admin:passw0rd ${repoUrl} --upload-file test-*.tgz -v'
 }
